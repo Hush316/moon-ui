f65f88a24cf3bbe871e8eb4fb9f7d0f889553bbd
cvbro <31604422@qq.com>
2022-10-22 23:24:36 +0800

    组件库初次提交

---
+++ src/entry.ts
@@ -0,0 +1,18 @@
+import { App } from 'vue'
+
+import MyButton from './button/Button'
+import SFCButton from './button/SFCButton.vue'
+import JSXButton from './button/JSXButton'
+
+// 导出单独组件
+export { MyButton, SFCButton, JSXButton }
+
+// 编写一个实现install的插件
+
+export default {
+  install(app: App): void {
+    app.component(MyButton.name, MyButton)
+    app.component(SFCButton.name, SFCButton)
+    app.component(JSXButton.name, JSXButton)
+  },
+}
---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+/// <reference types="vitest">
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx' //jsx插件
+
+// 引入Unocss插件
+import Unocss from './config/unocss'
+const rollupOptions = {
+  external: ['vue', 'vue-router'],
+  output: {
+    globals: {
+      vue: 'Vue',
+    },
+  },
+}
+export default defineConfig({
+  plugins: [vue(), vueJsx(), Unocss()],
+  build: {
+    rollupOptions,
+    minify: false,
+    cssCodeSplit: true,
+    lib: {
+      entry: './src/entry.ts',
+      name: 'MoonUI',
+      fileName: 'moon-ui',
+      // 导出模块格式
+      formats: ['es', 'esm', 'umd', 'iife'],
+    },
+  },
+  test:{
+    globals:true,
+    environment:'happy-dom',
+    transformMode:{
+      web:[/.[tj]sx$/]
+    }
+
+  }
+})
