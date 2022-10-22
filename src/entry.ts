fb57087e604b3d194bec87e8eed435db820257f3
cvbro <31604422@qq.com>
2022-10-23 00:32:05 +0800

    fix: 添加项目规范

--- src/entry.ts
+++ src/entry.ts
@@ -1,20 +1,20 @@
-import { App } from 'vue'
+import { App } from "vue";
 
-import MyButton from './button/Button'
+import MyButton from "./button/Button";
 
-import SFCButton from './button/SFCButton.vue'
+import SFCButton from "./button/SFCButton.vue";
 
-import JSXButton from './button/JSXButton'
+import JSXButton from "./button/JSXButton";
 
 // 导出单独组件
-export { MyButton, SFCButton, JSXButton }
+export { MyButton, SFCButton, JSXButton };
 
 // 编写一个实现install的插件
 
 export default {
   install(app: App): void {
-    app.component(MyButton.name, MyButton)
-    app.component(SFCButton.name, SFCButton)
-    app.component(JSXButton.name, JSXButton)
+    app.component(MyButton.name, MyButton);
+    app.component(SFCButton.name, SFCButton);
+    app.component(JSXButton.name, JSXButton);
   },
-}
+};
